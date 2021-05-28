c2642f715a08503cd204e7ed254af5d208ab9121
Kirichenko Nikita <knick@MacBook-Pro.local>
2021-05-28 14:52:55 +0300

    second step of project

--- src/manual.js
+++ src/manual.js
@@ -2,6 +2,9 @@ import program from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-V, --version', 'output the version number');
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format');
 
 export default program.parse();
