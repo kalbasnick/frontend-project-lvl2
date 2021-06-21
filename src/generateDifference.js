6cfb918c3d28a0a6a1cd18aa201725194a18f0c9
Kirichenko Nikita <knick@MacBook-Pro.local>
2021-06-21 20:31:47 +0300

    refactored generate generateDifference.js

--- src/generateDifference.js
+++ src/generateDifference.js
@@ -6,30 +6,25 @@ const genDiff = (file1, file2, formatName) => {
   const iter = (data1, data2) => {
     const keys = _.sortBy(_.uniq([...Object.keys(data1), ...Object.keys(data2)]));
 
-    return keys.reduce((acc, key) => {
+    return keys.map((key) => {
       if (isObject(data1[key]) && isObject(data2[key])) {
-        acc.push([key, iter(data1[key], data2[key]), { status: 'innerPropertyMatch' }]);
-        return acc;
+        return [key, iter(data1[key], data2[key]), { status: 'innerPropertyMatch' }];
       }
       if (!_.has(data2, key)) {
-        acc.push([key, data1[key], { status: 'removed' }]);
-        return acc;
+        return [key, data1[key], { status: 'removed' }];
       }
       if (!_.has(data1, key)) {
-        acc.push([key, data2[key], { status: 'added' }]);
-        return acc;
+        return [key, data2[key], { status: 'added' }];
       }
       if (data1[key] === data2[key]) {
-        acc.push([key, data2[key], { status: 'unchanged' }]);
-        return acc;
+        return [key, data2[key], { status: 'unchanged' }];
       }
       if (data1[key] !== data2[key]) {
-        acc.push([key, [data1[key], data2[key]], { status: 'changed' }]);
-        return acc;
+        return [key, [data1[key], data2[key]], { status: 'changed' }];
       }
 
-      return acc;
-    }, []);
+      return key;
+    });
   };
 
   return selectFormat(iter(file1, file2), formatName);
