8505dbc2b11a551d0e77f8ff5adb35c2a89ccc00
Kirichenko Nikita <knick@MacBook-Pro.local>
2021-07-05 18:25:11 +0300

    fix parsers

--- src/parsers.js
+++ src/parsers.js
@@ -5,10 +5,10 @@ import yaml from 'js-yaml';
 export default (filepath) => {
   switch (path.extname(filepath)) {
     case '.json':
-      return JSON.parse(fs.readFileSync(path.resolve(process.cwd(), filepath), 'utf-8'));
+      return JSON.parse(fs.readFileSync(path.resolve(filepath), 'utf-8'));
     case '.yml':
     case '.yaml':
-      return yaml.load(fs.readFileSync(path.resolve(process.cwd(), filepath), 'utf-8'));
+      return yaml.load(fs.readFileSync(path.resolve(filepath), 'utf-8'));
     default:
       throw new Error(`Wrong file extension: ${path.extname(filepath)}! The file extension should be JSON or YAML`);
   }
