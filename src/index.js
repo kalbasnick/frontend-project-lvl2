a61ebe8df3f492d91b654ace639d72d70335881f
Kirichenko Nikita <knick@MacBook-Pro.local>
2021-07-05 21:17:17 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import genDiff from './generateDifference.js';
 import selectFormat from './formatters/index.js';
 
-export default (filepath1, filepath2, formatName) => {
+export default (filepath1, filepath2, formatName = 'stylish') => {
   const result = genDiff(filepath1, filepath2);
   return selectFormat(result, formatName);
 };
