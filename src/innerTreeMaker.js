ef090dc4365de737ab58a59b28b09757fcc4da8f
Kirichenko Nikita <knick@MacBook-Pro.local>
2021-07-29 22:48:05 +0300

    error correction with mentor #2

--- src/innerTreeMaker.js
+++ src/innerTreeMaker.js
@@ -27,31 +27,3 @@ export default (parsedData1, parsedData2) => {
 
   return iter(parsedData1, parsedData2);
 };
-
-// export default (parsedData1, parsedData2) => {
-//   const iter = (data1, data2) => {
-//     const keys = _.sortBy(_.uniq([...Object.keys(data1), ...Object.keys(data2)]));
-
-//     return keys.map((key) => {
-//       if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-//         return [key, iter(data1[key], data2[key]), { status: 'nested' }];
-//       }
-//       if (!_.has(data2, key)) {
-//         return [key, data1[key], { status: 'removed' }];
-//       }
-//       if (!_.has(data1, key)) {
-//         return [key, data2[key], { status: 'added' }];
-//       }
-//       if (_.isEqual(data1[key], data2[key])) {
-//         return [key, data2[key], { status: 'unchanged' }];
-//       }
-//       if (data1[key] !== data2[key]) {
-//         return [key, [data1[key], data2[key]], { status: 'changed' }];
-//       }
-
-//       return data1[key];
-//     });
-//   };
-
-//   return iter(parsedData1, parsedData2);
-// };
